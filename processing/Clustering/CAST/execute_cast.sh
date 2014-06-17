832b08ec07360b87301049bc00b87b72515a183a
pdt <duc-thanh.phan@univ-nantes.fr>
2014-06-17 16:48:55 +0200

    fix fu

--- processing/Clustering/CAST/execute_cast.sh
+++ processing/Clustering/CAST/execute_cast.sh
@@ -5,7 +5,7 @@ OUT_PATH=
 CAST=
 SIMI=
 MAX_DIS=
-EXE=e
+EXE=
 usage()
 {
 cat << EOF
@@ -74,6 +74,6 @@ do
     #eval  "./bed2clust.sh -i ${D}/casecontrol_filtered -o ${OUT_PATH} -g ${GENO_TYPE} -p ${PLINK}"
 done
 
-echo "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/CAST/${DATE}/clustering_${PARAMS}.txt"
-eval "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/CAST/${DATE}/clustering_${PARAMS}.txt"
+echo "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/CAST/${DATE}/CAST_clustering_${PARAMS}.txt snp_counts.txt"
+eval "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/CAST/${DATE}/CAST_clustering_${PARAMS}.txt snp_counts.txt"
 
