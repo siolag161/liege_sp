0928660689cdc4c1f68b4d9bc5b432539b312ccf
pdt <duc-thanh.phan@univ-nantes.fr>
2014-06-17 09:22:13 +0200

    fix a minor bug in DBSCAN Script

--- processing/Clustering/DBSCAN/execute_dbscan.sh
+++ processing/Clustering/DBSCAN/execute_dbscan.sh
@@ -73,11 +73,11 @@ fi
 DATE=$(date +"%Y_%m_%d")
 PARAMS="${MIN_PTS}_${EPS}_${MAX_POS}"
 for D in `find ${IN_PATH} -type d`
-do_
+do
     echo $D
     CHR_DIR=$(basename $D)
     echo "${EXE}/clust_dbscan -i ${D}/casecontrol_filtered_data.csv -l ${D}/casecontrol_filtered_label.csv -o ${OUT_PATH}/DBSCAN/${DATE}/${CHR_DIR}_${PARAMS}.txt -e ${EPS} -s ${SIMI} -m ${MIN_PTS} -x ${MAX_POS} -v 1"
     eval "${EXE}/clust_dbscan -i ${D}/casecontrol_filtered_data.csv -l ${D}/casecontrol_filtered_label.csv -o ${OUT_PATH}/DBSCAN/${DATE}/${CHR_DIR}_${PARAMS}.txt -e ${EPS} -s ${SIMI} -m ${MIN_PTS} -x ${MAX_POS} -v 1"
-don
+done
 echo "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/DBSCAN/${DATE}/clustering_${PARAMS}.txt"
 eval "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/DBSCAN/${DATE}/clustering_${PARAMS}.txt"
