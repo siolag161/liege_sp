9e520d595311bdfc549a2b7a1e7781811a5d8232
pdt <duc-thanh.phan@univ-nantes.fr>
2014-06-17 07:46:44 +0200

    fixed minor scripts

--- processing/Clustering/CAST/execute_cast.sh
+++ processing/Clustering/CAST/execute_cast.sh
@@ -70,10 +70,10 @@ for D in `find ${IN_PATH} -type d`
 do
     CHR_DIR=$(basename $D)
     echo "${EXE}/clust_cast -i ${D}/casecontrol_filtered_data.csv -l ${D}/casecontrol_filtered_label.csv -o ${OUT_PATH}/CAST/${CHR_DIR}_${PARAMS}.txt -c ${CAST} -s ${SIMI} -m ${MAX_DIST} -v 1 "
-    # eval "${EXE}/clust_cast -i ${D}/casecontrol_filtered_data.csv -l ${D}/casecontrol_filtered_label.csv -o ${OUT_PATH}/CAST/${DATE}/${CHR_DIR}_${PARAMS}.txt -c ${CAST} -s ${SIMI} -m ${MAX_DIST} -v 1 "
+    eval "${EXE}/clust_cast -i ${D}/casecontrol_filtered_data.csv -l ${D}/casecontrol_filtered_label.csv -o ${OUT_PATH}/CAST/${DATE}/${CHR_DIR}_${PARAMS}.txt -c ${CAST} -s ${SIMI} -m ${MAX_DIST} -v 1 "
     #eval  "./bed2clust.sh -i ${D}/casecontrol_filtered -o ${OUT_PATH} -g ${GENO_TYPE} -p ${PLINK}"
 done
 
 echo "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/CAST/${DATE}/clustering_${PARAMS}.txt"
-
+eval "./fusionner.py  ${OUT_PATH}/CAST/${DATE}/ ${OUT_PATH}/CAST/${DATE}/clustering_${PARAMS}.txt"
 
