8d7f57a17a3a3b130c0936ceb6c32400b270c4ba
pdt <duc-thanh.phan@univ-nantes.fr>
2014-06-18 19:11:17 +0200

    fix max number of threads

--- KMedoids/main/main.cpp
+++ KMedoids/main/main.cpp
@@ -17,6 +17,7 @@
 #include <boost/accumulators/statistics/median.hpp>
 
 #include "kmedoids.hpp"
+#include <omp.h>
 
 namespace utl = utility;
 namespace po = boost::program_options;   
@@ -35,6 +36,11 @@ void saveClustering( const PAM& pam, const std::vector<unsigned>& ids, std::stri
  * @todo: merge this and CAST
  */
 int main(int argc, char** argv) {
+
+  int nProcessors=omp_get_max_threads();
+  omp_set_num_threads( std::max(nProcessors -3, 1) );
+  //std::cout<<omp_get_num_threads()<<std::endl;
+  
   utl::Timer timer, totalTimer; timer.start(); totalTimer.start();
 
   ApplicationOptions progOpt = getProgramOptions(argc, argv);  
